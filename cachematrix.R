49783ae4394146a3ce054b6606c6e58555190934
g9a2hvk9 <g9a2hvk9@gmail.com>
2019-11-16 09:43:41 +0100

    Adds completed programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -23,7 +23,7 @@ makeCacheMatrix <- function(x = matrix()) {
                 inv <<- NULL
         }
         get <- function() x
-        setinverse <- function(inverse) inv <<- inverse
+        setinverse <- A
         getinverse <- function() inv
         list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
